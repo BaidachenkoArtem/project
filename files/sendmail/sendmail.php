c4fc7359c80d6e8d74ad78936ff1f701c44591f8
Artem Baidachenko <123328374+BaidachenkoArtem@users.noreply.github.com>
2023-08-24 10:57:26 +0300

    Add files via upload

---
+++ files/sendmail/sendmail.php
@@ -0,0 +1,76 @@
+<?php
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'phpmailer/src/Exception.php';
+	require 'phpmailer/src/PHPMailer.php';
+	require 'phpmailer/src/SMTP.php';
+
+	$mail = new PHPMailer(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->setLanguage('ru', 'phpmailer/language/');
+	$mail->IsHTML(true);
+
+
+	$mail->isSMTP();                                            //Send using SMTP
+	$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+	$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+	$mail->Username   = 'haykljnoun@gmail.com';                     //SMTP username
+	$mail->Password   = 'vctvhknpnfcqolmv';                               //SMTP password
+	$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+	$mail->Port       = 465;                 
+
+
+	//Від кого лист
+	$mail->setFrom('haykljnoun@gmail.com', 'Фрілансер по життю'); // Вказати потрібний E-mail
+	//Кому відправити
+	$mail->addAddress('haykljnoun@gmail.com'); // Вказати потрібний E-mail
+	//Тема листа
+	$mail->Subject = 'Вітання! Це "Фрілансер по життю"';
+
+	//Тіло листа
+	$body = '<h1>Зустрічайте супер листа!</h1>';
+
+	if(trim(!empty($_POST['name']))){
+		$body.='<p><strong>Name:</strong>' . $_POST['name'].'</p>';
+	}	
+	if(trim(!empty($_POST['email']))){
+		$body.='<p><strong>E-mail:</strong>' . $_POST['email'].'</p>';
+	}	
+	if(trim(!empty($_POST['message']))){
+		$body.='<p><strong>Message:</strong>' . $_POST['message'].'</p>';
+	}	
+	if(trim(!empty($_POST['color']))){
+		$body.='<p><strong>Color:</strong>' . $_POST['color'].'</p>';
+	}
+	if(trim(!empty($_POST['colors']))){
+		$body.='<p><strong>Color:</strong>' . $_POST['colors'].'</p>';
+	}
+	/*
+	//Прикріпити файл
+	if (!empty($_FILES['image']['tmp_name'])) {
+		//шлях завантаження файлу
+		$filePath = __DIR__ . "/files/sendmail/attachments/" . $_FILES['image']['name']; 
+		//грузимо файл
+		if (copy($_FILES['image']['tmp_name'], $filePath)){
+			$fileAttach = $filePath;
+			$body.='<p><strong>Фото у додатку</strong>';
+			$mail->addAttachment($fileAttach);
+		}
+	}
+	*/
+
+	$mail->Body = $body;
+
+	//Відправляємо
+	if (!$mail->send()) {
+		$message = 'Помилка';
+	} else {
+		$message = 'Дані надіслані!';
+	}
+
+	$response = ['message' => $message];
+
+	header('Content-type: application/json');
+	echo json_encode($response);
+?>
